8f55b0eb3ea2f819357ddd4ff60aa8da20dfe6ed
Arunachalam K <arun2999@global.chainsys.com>
2022-03-11 10:17:56 +0530

    included ping page

---
+++ mentor/javascript/ping-coming-soon-page/script.js
@@ -0,0 +1,15 @@
+/* eslint-disable semi */
+/*eslint-disable*/
+"use strict";
+const inputEmail = document.getElementById("input-email");
+const submitBtn = document.getElementById("submit-btn");
+const form = document.getElementById("form");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputEmail.value.match(regEx)) {
+    form.classList.remove("error");
+  } else {
+    form.classList.add("error");
+  }
+});
