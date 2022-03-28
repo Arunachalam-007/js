7fbf6a0a70e8aff026850e181d3684a84672c282
Arunachalam K <arun2999@global.chainsys.com>
2022-03-28 07:20:39 +0530

    Included social media dashboard

---
+++ mentor/javascript/social-media-dashboard/script.js
@@ -0,0 +1,14 @@
+/* eslint-disable semi */
+/*eslint-disable*/
+var mycont = document.getElementById("themes");
+var checking = document.getElementById("check");
+
+function clicking() {
+  if (checking.checked == true) {
+    document.body.style.backgroundImage = `url('./images/light_bg.png')`;
+    mycont.setAttribute("data-theme", "light");
+  } else {
+    document.body.style.backgroundImage = `url('./images/bg.png')`;
+    mycont.setAttribute("data-theme", "dark");
+  }
+}
